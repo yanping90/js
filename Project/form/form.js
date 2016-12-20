4cfbcacb7ddd9665b8a2b4d7a1171206394f90cd
yanping90 <design@html.js.cn>
2016-12-20 22:17:02 +0800

    $(function(){})

--- Project/form/form.js
+++ Project/form/form.js
@@ -1,89 +1,98 @@
 /**
  * Created by etc on 2016/12/20.
  */
-$(function(){
-    var successText = "格式正确";
-    var errorText = "格式错误";
-    var nullText = "不能为空";
+$(function () {
+	var successText = "格式正确";
+	var errorText = "格式错误";
+	var nullText = "不能为空";
 
-    $("#companyname").on("change",function(){
-        var _self = $(this);
-        var noteSpan = _self.parent().children(".noteSpan");
-        noteSpan.removeClass("success").removeClass("error").show();
-        if(/旅行社有限公司$/.test(_self.val())){
-            noteSpan.addClass("success").html(successText);
-        }else if(_self.val() == ""){
-            noteSpan.addClass("error").html(nullText);
-        }else{
-            noteSpan.addClass("error").html(errorText);
-        }
-    });
+	$("#companyname").on("change", function () {
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		noteSpan.removeClass("success").removeClass("error").show();
+		if (/旅行社有限公司$/.test(_self.val())) {
+			noteSpan.addClass("success").html(successText);
+		} else if (_self.val() == "") {
+			noteSpan.addClass("error").html(nullText);
+		} else {
+			noteSpan.addClass("error").html(errorText);
+		}
+	});
 
-    $("#phone").on("change",function(){
-        var _self = $(this);
-        var noteSpan = _self.parent().children(".noteSpan");
-        if(/^[1][345678]\d{9}$/.test(_self.val())){
-            noteSpan.addClass("success").html(successText);
-        }else if(_self.val() == ""){
-            noteSpan.addClass("error").html(_self.attr("nullmsg"));
-        }else{
-            noteSpan.addClass("error").html(errorText);
-        }
-    });
+	$("#phone").on("change", function () {
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		if (/^[1][345678]\d{9}$/.test(_self.val())) {
+			noteSpan.addClass("success").html(successText);
+		} else if (_self.val() == "") {
+			noteSpan.addClass("error").html(_self.attr("nullmsg"));
+		} else {
+			noteSpan.addClass("error").html(errorText);
+		}
+	});
 
-    $("#password1").on("change",function(){
-        var _self = $(this);
-        var noteSpan = _self.parent().children(".noteSpan");
-        noteSpan.removeClass("success").removeClass("error").show();
-        var passText = _self.val();
-        if(passText.length >=6 && passText.length <=16){
-            if(/\s+/.test(passText)){
-                noteSpan.addClass("error").html(_self.attr("errormsg"));
-            }else if(/\d{9}/.test(passText)){
-                noteSpan.addClass("error").html(_self.attr("errormsg"));
-            }else{
-                noteSpan.addClass("success").html(successText);
-            }
-        }else{
-            noteSpan.addClass("error").html(_self.attr("datatype"));
-        }
-    });
+	$("#password1").on("change", function () {
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		noteSpan.removeClass("success").removeClass("error").show();
+		var passText = _self.val();
+		if (passText.length >= 6 && passText.length <= 16) {
+			if (/\s+/.test(passText)) {
+				noteSpan.addClass("error").html(_self.attr("errormsg"));
+			} else if (/\d{9}/.test(passText)) {
+				noteSpan.addClass("error").html(_self.attr("errormsg"));
+			} else {
+				noteSpan.addClass("success").html(successText);
+			}
+		} else {
+			noteSpan.addClass("error").html(_self.attr("datatype"));
+		}
+	});
 
-    $("#password2").on("change",function(){
-        var _self = $(this);
-        var noteSpan = _self.parent().children(".noteSpan");
-        noteSpan.removeClass("success").removeClass("error").empty();
-        if($("#password1").val() !== _self.val()){
-            noteSpan.addClass("error").html("两次输入的密码不一致，请重新输入");
-        }else{
-            noteSpan.addClass("success").html(successText);
-        }
-    });
+	$("#password2").on("change", function () {
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		noteSpan.removeClass("success").removeClass("error").empty();
+		if ($("#password1").val() !== _self.val()) {
+			noteSpan.addClass("error").html("两次输入的密码不一致，请重新输入");
+		} else {
+			noteSpan.addClass("success").html(successText);
+		}
+	});
 
-    $(".myform").on("submit",function(e){
-        if(! /[旅行社有限公司]$/.test($("#companyname").val())){
-            if($("#companyname").val() ==""){
-                $(".noteSpan").eq(0).addClass("error").html(nullText);
-            }
-            e.preventDefault();
-        }
-        if(! /^[1][345678]\d{9}$/.test($("#phone").val())){
-            if($("#phone").val() ==""){
-                $(".noteSpan").eq(1).addClass("error").html(nullText);
-            }
-            e.preventDefault();
-        }
-        var passText = $("#password1").val();
-        if(passText.length <6 || passText.length >16 || /\s+d{9}/.test(passText)){
-            if(passText ==""){
-                $(".noteSpan").eq(2).addClass("error").html($("#password1").attr("nullmsg"));
-            }
-            e.preventDefault();
-        }
-        if($("#password1").val() !== $("#password2").val()){
-            e.preventDefault();
-        }
-    });
+	$(".myform").on("submit", function (e) {
+		e.preventDefault();
+		$.get("/test.php"
+				,$(this).serialize()
+				,function(){
+				alert("提交成功"); //提交成功之后的信息
+				console.log(arguments);
+			}
+			,"json"
+		);
+		if (!/旅行社有限公司$/.test($("#companyname").val())) {
+			if ($("#companyname").val() == "") {
+				$(".noteSpan").eq(0).addClass("error").html(nullText);
+			}
+			e.preventDefault();
+		}
+		if (!/^[1][345678]\d{9}$/.test($("#phone").val())) {
+			if ($("#phone").val() == "") {
+				$(".noteSpan").eq(1).addClass("error").html(nullText);
+			}
+			e.preventDefault();
+		}
+		var passText = $("#password1").val();
+		if (passText.length < 6 || passText.length > 16 || /\s+d{9}/.test(passText)) {
+			if (passText == "") {
+				$(".noteSpan").eq(2).addClass("error").html($("#password1").attr("nullmsg"));
+			}
+			e.preventDefault();
+		}
+		if ($("#password1").val() !== $("#password2").val()) {
+			e.preventDefault();
+		}
+	});
 });
 
 
--- test.php
+++ test.php
@@ -1,4 +1,3 @@
-<pre>
 <?php
-
+header("Content-type: application/json; charset=UTF-8");
 echo json_encode($_REQUEST, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
