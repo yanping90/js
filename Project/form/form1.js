62b687031a192b278c62eaf3133fdbe8f0eeaf32
yanping90 <design@html.js.cn>
2016-12-21 21:53:36 +0800

    判断用户名的命名规则，length从0到大判断，用一级if else即可

--- Project/form/form1.js
+++ Project/form/form1.js
@@ -3,58 +3,55 @@
  */
 //可输入英文字母、数字，需以字母开头；长度4-20！
 
-$(function(){
-    $("#username").on("change",function(){
-        var _self = $(this);
-        var noteSpan = _self.parent().children(".noteSpan");
-        noteSpan.removeClass("success").removeClass("error").show();
-        var passText = _self.val();
-        if(_self.val().length>4){
-            if(/^[a-z]+[a-z0-9]+$/i.test(passText)){
-                noteSpan.addClass("success").html("正确");
-            }else{
-                noteSpan.addClass("error").html("格式错误");
-            }
-        }else{
-            noteSpan.addClass("error").html("请输入4-20个字符");
-        }
-        noteSpan.removeClass("success").removeClass("error").show();
-        $.ajax({
-            url:"/user.php",
-            data:{
-                action:"add",
-                username:$("#username").val()
-            },
-            success:function(r){
-                if(r.code) {
-                    $(".noteSpan").addClass("error").html(r.message +"，请重新填写");
-                }else{
-                    $(".noteSpan").addClass("success").html(r.message);
-                }
-            },
-        dataType:"json",
-            async:false
-            });
-    });
+$(function () {
+	$("#username").on("change", function () {
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		noteSpan.removeClass("success").removeClass("error").show();
+		var passText = _self.val();
+		if (_self.val().length > 4) {
+			if (/^[a-z]+[a-z0-9]+$/i.test(passText)) {
+				noteSpan.addClass("success").html("正确");
+			} else {
+				noteSpan.addClass("error").html("格式错误");
+			}
+		} else {
+			noteSpan.addClass("error").html("请输入4-20个字符");
+		}
+		noteSpan.removeClass("success").removeClass("error").show();
+		$.get("/user.php",
+			{
+				username:$("#username").val()
+			},
+		function (r) {
+			if (r.code) {
+				$(".noteSpan").addClass("success").html("用户名可用");
+			} else {
+				$(".noteSpan").addClass("error").html(r.message);
+			}
+		},
+		"json"
+		);
+});
 
-    $(".myform").on("submit",function(e){
-        e.preventDefault();
-        $.get("/user.php",
-            {
-                username:$("#username").val()
-            },
-            function(r){
-                if(r.code) {
-                    $(".noteSpan").addClass("error").html(r.message +"，请重新填写");
-                }else{
-                    $(".noteSpan").addClass("success").html(r.message);
-                }
-            },
-            "json"
-        );
-        $(".noteSpan").removeClass("error").removeClass("success").empty();
-        if($("#username").val().length<4 || !/^[a-z]+[a-z0-9]+$/i.test($("#username").val())){
-            e.preventDefault();
-        }
-    });
+$(".myform").on("submit", function (e) {
+	e.preventDefault();
+	$.get("/user.php",
+		{
+			username: $("#username").val()
+		},
+		function (r) {
+			if (r.code) {
+				$(".noteSpan").addClass("error").html(r.message + "，请重新填写");
+			} else {
+				$(".noteSpan").addClass("success").html(r.message);
+			}
+		},
+		"json"
+	);
+	$(".noteSpan").removeClass("error").removeClass("success").empty();
+	if ($("#username").val().length < 4 || !/^[a-z]+[a-z0-9]+$/i.test($("#username").val())) {
+		e.preventDefault();
+	}
+});
 })
---
+++ Project/form/user.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Chiao on 2016/12/21.
+ */
+$(function () {
+
+	$("#username").on("change", function () {
+
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		var userText = _self.val().replace(/\s+/g, "");
+		noteSpan.removeClass("success").removeClass("error").empty();
+		if (userText.length = 0) {
+			noteSpan.addClass("error").html("用户名不能为空");
+			return;
+		} else if (userText.length < 4) {
+			noteSpan.addClass("error").html("用户名长度为4-20");
+			return;
+		} else if (!/^[a-z]+[a-z0-9]+$/i.test(userText)) {
+			noteSpan.addClass("error").html("输入英文字母、数字，需以字母开头；长度4-20");
+			return;
+		}
+
+	});
+
+	$("#myform").on("submit",function(e){
+		e.preventDefault();
+		var userText = $("#username").val();
+		if(userText.length == 0
+			|| userText.length < 4
+			|| !/^[a-z]+[a-z0-9]+$/i.test(userText)){
+			return;
+		}
+
+		$.post("/user.php"
+			, {
+				action: "add"
+				,username: userText
+			}
+			, function(){
+				alert(arguments[0].message);
+			}
+			, "json"
+		);
+	});
+
+})
