1827f751f2c9b5f86280126bcd2e6588709725d3
yanping90 <design@html.js.cn>
2016-12-22 21:28:26 +0800

    调用了插件linkage callback的使用

--- Project/form/form.js
+++ Project/form/form.js
@@ -3,79 +3,89 @@
  */
 $(function () {
 
-    $("#username").on("change",function(){
-        var _self = $(this);
-        var noteSpan = _self.parent().children(".noteSpan");
-        noteSpan.removeClass("success").removeClass("error").show();
-        var userText = (_self.val()).replace(/\s+/i,"");
-        if(userText.length === 0){
-            noteSpan.addClass("error").html(_self.attr("nullmsg"));
-            return;
-        } else if (userText.length < 4){
-            noteSpan.addClass("error").html(_self.attr("errormsg"));
-            return;
-        } else if (/^[a-z]+[a-z0-9]+$/i.test(userText)){
-            //noteSpan.addClass("success").html(_self.attr("sucmsg"));
-        } else {
-            noteSpan.addClass("error").html(_self.attr("errormsg"));
-            return;
-        }
+	$(".area").linkage(
+		{
+			newSubmenu: "subMenu"
+			, callback: function () {
+				$("#provice").val($(".linkage-btn").eq(0).text());
+				$("#city").val($(".linkage-btn").eq(1).text());
+				$("#area").val($(".linkage-btn").eq(2).text());
+			}
+		}
+	);
+	$("#username").on("change", function () {
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		noteSpan.removeClass("success").removeClass("error").show();
+		var userText = (_self.val()).replace(/\s+/i, "");
+		if (userText.length === 0) {
+			noteSpan.addClass("error").html(_self.attr("nullmsg"));
+			return;
+		} else if (userText.length < 4) {
+			noteSpan.addClass("error").html(_self.attr("errormsg"));
+			return;
+		} else if (/^[a-z]+[a-z0-9]+$/i.test(userText)) {
+			//noteSpan.addClass("success").html(_self.attr("sucmsg"));
+		} else {
+			noteSpan.addClass("error").html(_self.attr("errormsg"));
+			return;
+		}
 
-        $.get("/user.php"
-            , {
-                username: userText
-            }
-            , function(r){
-                if(r.code){
-                    noteSpan.addClass("success").html(_self.attr("sucmsg"));
-                }else{
-                    noteSpan.addClass("error").html(_self.attr("definemsg"));
-                }
-            }
-            , "json"
-        );
-    });
+		$.get("/user.php"
+			, {
+				username: userText
+			}
+			, function (r) {
+				if (r.code) {
+					noteSpan.addClass("success").html(_self.attr("sucmsg"));
+				} else {
+					noteSpan.addClass("error").html(_self.attr("definemsg"));
+				}
+			}
+			, "json"
+		);
+	});
 
-    $("#phone").on("change",function(){
-        var _self = $(this);
-        var noteSpan = _self.parent().children(".noteSpan");
-        var phoneText = _self.val();
-        if(phoneText.length === 0){
-            noteSpan.addClass("error").html(_self.attr("nullmsg"));
-            return;
-        }else if(/^1[3,4,5,7][0-9]{9}$/.test(phoneText)){
-            noteSpan.addClass("success").html(_self.attr("sucmsg"));
-        }else{
-            noteSpan.addClass("error").html(_self.attr("errormsg"));
-            return;
-        }
-    });
-    $(".myform").on("submit", function (e) {
-        e.preventDefault();
-        var userText = $("#username").val();
-        if(userText.length === 0
-            || userText.length <4
-            || !/^[a-z]+[a-z0-9]+$/i.test(userText)){
-            return;
-        }
+	$("#phone").on("change", function () {
+		var _self = $(this);
+		var noteSpan = _self.parent().children(".noteSpan");
+		var phoneText = _self.val();
+		if (phoneText.length === 0) {
+			noteSpan.addClass("error").html(_self.attr("nullmsg"));
+			return;
+		} else if (/^1[3,4,5,7][0-9]{9}$/.test(phoneText)) {
+			noteSpan.addClass("success").html(_self.attr("sucmsg"));
+		} else {
+			noteSpan.addClass("error").html(_self.attr("errormsg"));
+			return;
+		}
+	});
+	$(".myform").on("submit", function (e) {
+		e.preventDefault();
+		var userText = $("#username").val();
+		if (userText.length === 0
+			|| userText.length < 4
+			|| !/^[a-z]+[a-z0-9]+$/i.test(userText)) {
+			return;
+		}
 
-        $.get("/user.php"
-            , {
-                username: $("#username").val()
-            }
-            , function(r){
-                if(r.code){
-                    alert("注册成功");
-                }
-            }
-            , "json"
-        );
+		$.get("/user.php"
+			, {
+				username: $("#username").val()
+			}
+			, function (r) {
+				if (r.code) {
+					alert("注册成功");
+				}
+			}
+			, "json"
+		);
 
-        var phoneText = $("#phone").val();
-        if(phoneText.length === 0
-            || !/^1[3,4,5,7][0-9]{9}$/.test(phoneText)){
-            return;
-        }
+		var phoneText = $("#phone").val();
+		if (phoneText.length === 0
+			|| !/^1[3,4,5,7][0-9]{9}$/.test(phoneText)) {
+			return;
+		}
 
 	});
 });
--- Project/linkage/linkage.js
+++ Project/linkage/linkage.js
@@ -2,93 +2,94 @@
  * Created by Chiao on 2016/12/18.
  */
 
-!function($){
-	$.fn.linkage = function(opt){
-		var settings = $.extend({}, $.fn.linkage.defaults,opt);
+!function ($) {
+	$.fn.linkage = function (opt) {
+		var settings = $.extend({}, $.fn.linkage.defaults, opt);
 		var linkage = ".linkage";
 		var menu = ".linkage-menu";
 		var btn = ".linkage-btn";
 		var subMenu = ".linkage-subMenu";
-        var disabled = "disabled";
-        var selected = "selected";
-        var defaultText = "请选择";
+		var disabled = "disabled";
+		var selected = "selected";
+		var defaultText = "请选择";
 
 		var subMenu = $("<ul>").addClass(subMenu.substr(1)).addClass(settings.newSubmenu).appendTo(document.body).hide();
-		return this.each(function(i,el){
+		return this.each(function (i, el) {
 				var _self = $(el);
-				for(i = 0;i<settings.levels;i++){
+				for (i = 0; i < settings.levels; i++) {
 					_self.append($("<div>").addClass(menu.substr(1)).addClass(settings.newMenu)
 						.append($("<a>").html(defaultText).addClass(btn.substr(1)).addClass(settings.newBtn)));
 				}
-                if(typeof settings.callback == "function"){
-                    settings.callback.call(_self);
-                }
-                _self.children(menu).slice(1).addClass(disabled).addClass(settings.newDisabled);
-                _self.children(menu).on("click",function(){
+				_self.children(menu).slice(1).addClass(disabled).addClass(settings.newDisabled);
+				_self.children(menu).on("click", function () {
 					var _self = $(this);
 					//生成数组列表
 					_self.append(subMenu.show().empty());
-					var arr1= settings.arr;
-                    var hasUndefined = false;
-                    $(el).children(menu).slice(0,_self.index()).each(function(i,el){
+					var arr1 = settings.arr;
+					var hasUndefined = false;
+					$(el).children(menu).slice(0, _self.index()).each(function (i, el) {
 						var _self = $(el);
-						if(_self.attr("select") == undefined){
-                            hasUndefined = true;
-                            return;
+						if (_self.attr("select") == undefined) {
+							hasUndefined = true;
+							return;
 						}
-                        _self.next().removeClass(disabled).removeClass(settings.newDisabled);
-                        arr1 = arr1[_self.attr("select")][2];
+						_self.next().removeClass(disabled).removeClass(settings.newDisabled);
+						arr1 = arr1[_self.attr("select")][2];
 					});
-                    if(hasUndefined){
-                        return;
-                    }
+					if (hasUndefined) {
+						return;
+					}
 					//生成html
-					$.each(arr1,function(i,v){
+					$.each(arr1, function (i, v) {
 						$("<li>")
-                            .append($("<a>").html(v[1]))
-                            .attr("idx",i)
-                            .attr("id",v[0])
-                            .appendTo(subMenu);
+							.append($("<a>").html(v[1]))
+							.attr("idx", i)
+							.attr("id", v[0])
+							.appendTo(subMenu);
 					});
 
-                    if(arr1.length === 1){
-                        subMenu.children().eq(0).trigger("click");
-                        _self.addClass(disabled).addClass(settings.newDisabled);
-                    }else{
-                        subMenu.children().eq(_self.attr("select")).addClass(selected).addClass(settings.newSelected);
-                    }
+					if (arr1.length === 1) {
+						subMenu.children().eq(0).trigger("click");
+						_self.addClass(disabled).addClass(settings.newDisabled);
+					} else {
+						subMenu.children().eq(_self.attr("select")).addClass(selected).addClass(settings.newSelected);
+					}
 				});
 
-                _self.children(subMenu).on("click","li",function(e){
-                    e.stopPropagation();
-                    var _self = $(this);
-                    //把选中的索引给按钮
-                    var parentBtn = _self.parents(menu);
-                    var hasChanged = parentBtn.attr("select") !== _self.attr("idx");
-                    parentBtn.attr("select",_self.attr("idx")).attr("id",_self.attr("id"));
-                    parentBtn.nextAll().each(function(i,el){
-                        var _self = $(el);
-                        if(hasChanged){
-                            _self.children(btn).html(defaultText);
-                            _self.addClass(disabled).addClass(settings.newDisabled);
-                        }
-                    });
-                    parentBtn.children(btn).html(_self.text());
-                    subMenu.hide();
-                    parentBtn.next().trigger("click");
-                });
+				_self.children(subMenu).on("click", "li", function (e) {
+					e.stopPropagation();
+					var _self = $(this);
+					//把选中的索引给按钮
+					var parentBtn = _self.parents(menu);
+					var hasChanged = parentBtn.attr("select") !== _self.attr("idx");
+					parentBtn.attr("select", _self.attr("idx")).attr("id", _self.attr("id"));
+					parentBtn.nextAll().each(function (i, el) {
+						var _self = $(el);
+						if (hasChanged) {
+							_self.children(btn).html(defaultText);
+							_self.addClass(disabled).addClass(settings.newDisabled);
+						}
+					});
+					parentBtn.children(btn).html(_self.text());
+					subMenu.hide();
+					parentBtn.next().trigger("click");
+					if (typeof settings.callback == "function") {
+						settings.callback.call(_self);
+					}
+				});
 
 			}
 		);
 	}
 	$.fn.linkage.defaults = {
 		levels: 3
-        ,newMenu: ""
-        ,newSubmenu: ""
-        ,newBtn: ""
-        ,newSelected: ""
-        ,newDisabled: ""
-        ,callback: function (){}
-        ,arr:arr
+		, newMenu: ""
+		, newSubmenu: ""
+		, newBtn: ""
+		, newSelected: ""
+		, newDisabled: ""
+		, callback: function () {
+		}
+		, arr: arr
 	};
 }(jQuery);
