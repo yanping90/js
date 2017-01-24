20606b5ab967aea9df70c0308194a735142e5b1d
etc <design@html.js.cn>
2017-01-24 17:28:39 +0800

    scroll

--- Project/scroll/js/scroll.js
+++ Project/scroll/js/scroll.js
@@ -2,9 +2,27 @@
  * Created by etc on 2017/1/23.
  */
 $(function(){
-    $(".scroll_bar").on("mousedown",function(){
-        var contHeight = $(".scroll_cont").children("p").height();
-        var scrollHeight = $(".scroll_cont").scrollTop(100);
-        console.log(contHeight);
+    $(".scroll").on("mousedown",".scroll_bar",function(e){
+        e.preventDefault();
+        var _self = $(this);
+        $(document).on("mousemove.scroll_cont",function(e){
+            //滑块移动距离
+            var slideHeight = e.pageY;
+            //滑块可移动距离
+            var slideHeights = $(".scroll_cont").height() - $(".scroll_slide").height();
+            //内容可视区高度
+            var contHeight = $(".scroll_cont").height();
+            //内容可移动距离
+            var contHeights = $(".scroll_cont p").height() - contHeight;
+            //内容滚动高度
+            var scrollContHeight = (slideHeight * contHeights)/slideHeights;
+            $(".scroll_cont").scrollTop(scrollContHeight);
+            //滑块移动距离
+            var scrollContHeights = $(".scroll_cont").scrollTop();
+            var slideDistance = (slideHeights * scrollContHeights)/contHeights;
+            $(".scroll_slide").css("marginTop",slideDistance + "px");
+        }).on("mouseup.scroll_cont",function(){
+            $(document).off(".scroll_cont");
+        });
     });
 })
