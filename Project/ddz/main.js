2ba8086e27de2d2c7297627a2b34c9fd6cd2ebe4
yanping90 <design@html.js.cn>
2017-02-26 20:04:16 +0800

    callback  countDown

--- Project/ddz/main.js
+++ Project/ddz/main.js
@@ -1,24 +1,48 @@
 $(function(){
 
-	var changeJSON = function(obj){
-		return JSON.stringify(obj);
-	};
+	//显示总牌数
+	$(".sheet").on("cards:sheet",function(e,arr){
+		$(this).html(arr.length);
+	});
+	var arrSheet = [1,2];
+	$(".sheet").trigger("cards:sheet",[arrSheet]);
 
-	var changeObj = function(json){
-		return JSON.parse(json);
-	};
+	//牌友名字显示
+	$(".name").on("actor:name",function(e,id){
+		$(this).html(id.substr(1));
+	});
+	$(".name").trigger("actor:name",["#actor01"]);
 
-	//进房间
-	//服务器传过来的JSON信息
-	var roomInfo ='{"action":"room","id":"actor01"}';
-	//转化为Object
-	var dataRoom=changeObj(roomInfo);
-	var id = dataRoom.id;
-	$(".btn").eq(id.substr(id.length -1) - 1).on("click",function(){
-		$(this).attr("id",id).attr("href","ready.html");
+	//显示牌
+	$(".showCards").on("cards:show",function(e,arr){
+		for(var i=0;i<arr.length;i++){
+			$(this).append($("<a>").html(arr[i]));
+		}
+	});
+	$("#showCards").on("click",function(){
+		var arrCards =[14,10,3,16];
+		$(".showCards").trigger("cards:show",[arrCards]);
 	});
 
-	//准备中
+	//倒计时
+	function CountDown(n, cb) {
+		this.n = n;
+		this.callback = cb;
+		var self = this;
+		this.tick = function () {
+			// before hook
+			if (-- self.n <= 0) {
+				clearInterval(self.timer);
+			}
+			// after hook
+			self.callback.apply(self, [self.n]);
+		}
+		this.timer = setInterval(this.tick, 1000);
+	}
+
+	//打上家牌
+	var lastCards;
+	var currCards;
 
 
-})
+});
