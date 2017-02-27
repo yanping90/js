0ca9aee23da35315f5c1f73993611bd471f84cf9
yanping90 <design@html.js.cn>
2017-02-27 21:41:15 +0800

    dot

--- Project/scroll_photo/main.js
+++ Project/scroll_photo/main.js
@@ -6,9 +6,13 @@ $(function () {
 			steps=steps-6;
 		} else{
 			steps =0;
+			clearInterval(int);
+			setTimeout(function(){
+				int = setInterval(scroll,10);
+			},3000);
 			$(".scroll").children().eq(0).appendTo($(".scroll"));
 		}
 		steps = $(".scroll").css("margin-left",steps+"px");
 	}
-	setInterval(scroll,10);
+	var int =setInterval(scroll,10);
 })
