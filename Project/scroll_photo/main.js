2a6e7592b814a054f3f7b4a6cc171f087565878d
yanping90 <design@html.js.cn>
2017-02-28 16:02:34 +0800

    dot

--- Project/scroll_photo/main.js
+++ Project/scroll_photo/main.js
@@ -4,6 +4,8 @@ $(function () {
 		var steps = parseInt($(".scroll").css("margin-left"));
 		if(steps>-520){
 			steps=steps-6;
+			$(".select").removeClass("select");
+			$(".scroll_num").children().eq($(".scroll").children().eq(0).attr("id")).addClass("select");
 		} else{
 			steps =0;
 			clearInterval(int);
