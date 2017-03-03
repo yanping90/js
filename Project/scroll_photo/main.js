11154ddd8687cd0c5cc3391613473e0ebee3a5f4
yanping90 <252602965@qq.com>
2017-03-03 14:14:42 +0800

    clearTimeout

--- Project/scroll_photo/main.js
+++ Project/scroll_photo/main.js
@@ -1,6 +1,6 @@
 $(function () {
     var timer;
-	var scroll = function(){
+	var leftScroll = function(){
 		var steps = parseInt($(".scroll").css("margin-left"));
 		if(steps>-520){
 			steps=steps-6;
@@ -16,6 +16,23 @@ $(function () {
 		}
 		steps = $(".scroll").css("margin-left",steps+"px");
 	}
+    var rightScroll = function(){
+        var steps =parseInt($(".scroll").css("margin-left"));
+        if(steps < 520){
+            steps = steps +6;
+            $(".select").removeClass("select");
+            $(".scroll_num").children().eq($(".scroll").children().eq(0).attr("id")).addClass("select");
+        } else {
+            steps =0;
+            clearInterval(int);
+            timer=setTimeout(function(){
+                int = setInterval(scroll,10);
+            },3000);
+            $(".scroll").children().eq(0).appendTo($(".scroll"));
+        }
+    }
+    var scroll;
+    if(){}
     var int =setInterval(scroll,10);
     $(".scroll_box").on("click:roll", function () {
         clearInterval(int);
