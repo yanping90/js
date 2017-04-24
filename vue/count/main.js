175e6f5f761a803e9adc8770df8f3816c9b4472d
√.Smileつ <252602965@qq.com>
2017-04-24 13:02:57 +0800

    countDown

--- vue/count/main.js
+++ vue/count/main.js
@@ -3,20 +3,38 @@ window.onload= function(){
     vm1 = new Vue({
         el:"#time",
         data:{
-            result:"xx"
+            day:"",
+            hour:"",
+            minutes:"",
+            second:""
         },
         computed:{
             showDay:function(){
                 var _self=this;
                 var roll=function(){
-                    var resultD= new Date("2017-4-27").getTime();
+                    var resultD= new Date("2017-4-30").getTime();
                     var nowD = new Date().getTime();
                     var leftD = (resultD-nowD)/1000;
+                    if(leftD<0){
+                        console.log("日期错误");
+                        return;
+                    }
                     var day = (leftD/(60*60*24)) >> 0;
                     var hour = (leftD/(60*60) % 24) >> 0;
                     var minutes = (leftD/60 % 60) >> 0;
                     var second = (leftD % 60) >> 0;
-                    _self.result ="还剩" + day + "天" + hour + "小时" + minutes + "分" + second + "秒";
+                    var format = function(a){
+                        if(a.toString().length==1){
+                            a = "0"+a;
+                        } else{
+                            a=a;
+                        }
+                        return a;
+                    }
+                    _self.day=day;
+                    _self.hour=hour;
+                    _self.minutes=format(minutes);
+                    _self.second=format(second);
                 }
                 setInterval(roll,500);
             }
