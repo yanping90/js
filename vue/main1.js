af79f5b31dde24e1bbb7ac4f7a8d6cc41d18b575
√.Smileつ <252602965@qq.com>
2017-04-25 15:23:30 +0800

    .textContent

--- vue/main1.js
+++ vue/main1.js
@@ -1,12 +1,23 @@
 window.onload=function(){
 
-    var d3= new Vue({
-        el:"#demo3",
+
+    d4=new Vue({
+        el:"#dem04",
         data:{
-            message:"hello world"
+            msg:"123"
         }
     });
+    d4.msg="222123456";
 
+    d3= new Vue({
+        el:"#demo3",
+        data:{
+            message:"hello world",
+            o1:""
+        }
+    });
+    d3.o1={};
+    Vue.set(d3.o1,"b","how old are you?");
 
     var d1=new Vue({
         el:"#demo2",
