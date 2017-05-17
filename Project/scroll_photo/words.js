896f9dbf4bff8adb742a6adcb4b1887eff322eee
√.Smileつ <252602965@qq.com>
2017-05-17 15:05:38 +0800

    self

---
+++ Project/scroll_photo/words.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    var autoTimer=0;
+    var mt=0;
+    var $wordsList = $(".wordsList");
+    $wordsList.on("scrollTop",function(){
+        var timer = setInterval(function(){
+            mt-=1;
+            if(mt<-40){
+                mt=0;
+                clearInterval(timer);
+                $wordsList.append($wordsList.children().first());
+            }
+            $wordsList.css("marginTop",mt+"px");
+
+        },10);
+    });
+    $wordsList.on("mouseleave",function(){
+        autoTimer = setInterval(function(){
+            $wordsList.trigger("scrollTop");
+        },4000);
+    });
+    $wordsList.on("mouseenter",function(){
+        clearInterval(autoTimer);
+    });
+
+    $wordsList.trigger("mouseleave");
+
+})
